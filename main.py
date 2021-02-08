b9b63980282879007d0021437c95f2ea7d608d9c
root <root@instance-1.us-central1-a.c.angelic-bond-302917.internal>
2021-02-08 16:08:08 +0000

    add timings

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import h5py
 import os
 from pymongo import MongoClient
 import zlib
+import time
+from typing import *
 
 app = FastAPI(title="BetterReads API")
 
@@ -92,20 +94,37 @@ db_pwd = os.getenv('MONGO_PWD')
 client = MongoClient(f'mongodb+srv://cdminix:{db_pwd}@cluster0.pdjrf.mongodb.net/Reviews_Data?retryWrites=true&w=majority')
 db = client.Reviews_Data
 
+def elapsed_time():
+    e_time = time.time()
+    if not hasattr(elapsed_time, 's_time'):
+        elapsed_time.s_time = e_time
+    else:
+        time_diff = round(1000 * (e_time - elapsed_time.s_time), 2)
+        elapsed_time.s_time = e_time
+        return time_diff
+    return None
+
 @app.get("/search/{query}")
-def get_query(query):
+def get_query(query: str, measure_time: Optional[bool] = False):
+    if measure_time:
+        elapsed_time()
+        time_dict = {}
     query = model.encode([query])[0]
+    if measure_time:
+        time_dict['encoding'] = elapsed_time()
     hashed, vectors = lsh.get(query)
+    if measure_time:
+        time_dict['get_bucket'] = elapsed_time()
     ids = [f"{hashed}_{int(id)}" for id in nn.get_nn(lsh.quantize(query), vectors)]
+    if measure_time:
+        time_dict['nn_search'] = elapsed_time()
     sents = list(db['sentence_data'].find({"_id": {"$in": ids}}))
-    #print(sents)
     review_ids = [s['review'] for s in sents]
     reviews = list(db['review_data'].find({"_id": {"$in": review_ids}}))
-    #print(reviews)
     isbns = [r['isbn'] for r in reviews]
     books = list(db['book_data'].find({"isbn": {"$in": isbns}}))
-    #print(books)
     results = {}
+    time_dict['db_calls'] = elapsed_time()
     for s in sents:
         review = [r for r in reviews if s['review'] == r['_id']][0]
         book = [b for b in books if b['isbn'] == review['isbn']][0]
@@ -119,4 +138,7 @@ def get_query(query):
             'image': book['image_url'],
             'title': book['title'],
         }
+    time_dict['finalize_and_decompress'] = elapsed_time()
+    time_dict['total'] = sum(time_dict.values())
+    results['timings'] = time_dict
     return results
