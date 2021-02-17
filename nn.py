Fixed NN overflow

nn.py
@@ -9,9 +9,10 @@ class NN():
     
     # Returns Euclidean distance between vector and matrix of vectors
     def _matrix_distance_(self, v1, matrix):
-        v1 = np.array([v1])
-        matrix = np.matrix(matrix)
-
+        # Conversion to float prevents overflow from following operations
+        v1 = np.array([v1]).astype("float")
+        matrix = matrix.astype("float")
+        
         p1 = np.sum(v1**2, axis=1)[:, np.newaxis]
         p2 = np.sum(matrix**2, axis=1)
         p3 =  -2 * np.dot(v1, matrix.T)
@@ -40,14 +41,18 @@ class NN():
                         dists[max_idx] = dist
                         max_dist = np.max(dists)
 
-        print(index)
+        print(dists)
         return index[~np.isnan(index)]
 
     # Updated kNN for matrix ops
     def get_k_nn(self, v1, vectors, k=10):
         print(f'searching {len(vectors)} vectors')
+        vectors = np.array(vectors)
 
         dists = self._matrix_distance_(v1, vectors)
         top_k = np.argsort(dists, axis=1)[0,:k]
         
-        return vectors[top_k[:],:]
+        #results = vectors[top_k[:],:]
+        results = top_k
+
+        return results